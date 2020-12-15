678a57e9c6ea0594b94257eb7ef83914cd87c46a
ASlawinska <slawinska.adrianna@gmail.com>
2020-12-15 15:04:45 +0100

    Added new exercises contain add, remove and toggle class.

--- DOM _addEvenListener/js/main.js
+++ DOM _addEvenListener/js/main.js
@@ -1,5 +1,14 @@
 //pobieranie przycisku
-const btn1 = document.querySelector('.btn1')
-const btn2 = document.querySelector('.btn2')
-const btn3 = document.querySelector('.btn3')
+const btnAdd = document.querySelector('.btn1');
+const btnRemove = document.querySelector('.btn2');
+const btnToggle = document.querySelector('.btn3');
+const text = document.querySelector('p');
 
+const addFunct = () => {
+    text.classList.add('new-color')
+}
+btnAdd.addEventListener('click', addFunct);
+
+btnRemove.addEventListener('click', function() { text.classList.remove('new-color')})
+
+btnToggle.addEventListener('click',() => text.classList.toggle('new-color'));
