15b9e0de4e7310534c2579facdad98117caec5c9
ASlawinska <slawinska.adrianna@gmail.com>
2020-12-19 14:26:36 +0100

    Added toggle button to add even listener.

--- DOM _zadania/js/main.js
+++ DOM _zadania/js/main.js
@@ -13,12 +13,9 @@
 //zad2
 const btn1 = document.querySelector('.btn1')
 const btn2 = document.querySelector('.btn2')
+const btn3 = document.querySelector('.btn3')
 const text =document.querySelector('.text')
-// function Show() {
-//     text.classList.add('show')
-//     text.classList.('show')
-    
-// }
+
 btn1.addEventListener('click', function () {
     text.classList.add('show')
     text.classList.remove('hide')
@@ -26,4 +23,8 @@ btn1.addEventListener('click', function () {
 btn2.addEventListener('click', function () {
     text.classList.add('hide')
     text.classList.remove('show')
+})
+// za pomocą jednego przycisku 
+btn3.addEventListener('click', function () {
+    text.classList.toggle('show')
 })
