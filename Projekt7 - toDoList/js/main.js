9c55a3b5306ef7cf83bd370d1951731d58210f8a
ASlawinska <slawinska.adrianna@gmail.com>
2021-01-15 00:06:03 +0100

    Added code containd cancel function in edit section

--- Projekt7 - toDoList/js/main.js
+++ Projekt7 - toDoList/js/main.js
@@ -4,9 +4,13 @@ let $addBtn; // przycisk dodania tekstu
 let $ulList;// lista zadań
 let $newTask// nowe zadanie
 let $toolsPanel // div przechowujacy przyciski
-let $completeBtn // btn check
-let $editBtn // edit
-let $deleteBtn // delete
+
+let $popup // pobranie popub
+let $popupInfo; // alert w popup
+let $editedToDo; //edytowany to do
+let $popupInput; // tekst wpisywany do inputa w popupie
+let $addPopupBtn; // zawtierdz w popup
+let $closeToDoBtn; //przycisk zamykanie w popupie
 
 
 const main = () => {
@@ -19,15 +23,17 @@ const prepareDOMElements = () => {
     $alertInfo = document.querySelector('.alertInfo');
     $addBtn = document.querySelector('.addBtn');
     $ulList = document.querySelector('.todoList ul');
-    $toolsPanel =document.querySelector('.tools')
-    $$completeBtn =document.querySelector('.complete')
-    $editBtn =document.querySelector('.edit')
-    $deleteBt =document.querySelector('.delete')
+    $popup =document.querySelector('.popup')
+    $$popupInfo =document.querySelector('.popupInfo')
+    $popupInput =document.querySelector('.popupInput')
+    $addPopupBtn =document.querySelector('.accept')
+    $closeToDoBtn =document.querySelector('.cancel')
 };
 // pobieramy nasłuchiwanie
 const prepareDOMEvents = () => {
-    $addBtn.addEventListener('click', addNewTask)
-
+    $addBtn.addEventListener('click', addNewTask);
+    $ulList.addEventListener('click', checkClick);
+    $closeToDoBtn.addEventListener('click', closePopup);
 };
 
 const addNewTask = () => {
@@ -42,25 +48,43 @@ const addNewTask = () => {
         $alertInfo.innerHTML = 'Wpisz tekst zadania'
     }}
 const createToolsArea = () => {
-    $toolsPanel = document.createElement('div');
-    $toolsPanel.classList.add('tools');
-    $newTask.appendChild($toolsPanel);
+    const toolsPanel = document.createElement('div');
+    toolsPanel.classList.add('tools');
+    $newTask.appendChild(toolsPanel);
     
-    $completeBtn = document.createElement('button');
-    $completeBtn.classList.add('complete');
-    $completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete');
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>'
     
-    $editBtn = document.createElement('button');
-    $editBtn.classList.add('edit');
-    $editBtn.innerText = 'EDIT'
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit');
+    editBtn.innerText = 'EDIT'
     
-    $deleteBtn = document.createElement('button');
-    $deleteBtn.classList.add('delete');
-    $deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
 
-    $toolsPanel.appendChild($completeBtn);
-    $toolsPanel.appendChild($editBtn);
-    $toolsPanel.appendChild($deleteBtn);
+    toolsPanel.appendChild(completeBtn);
+    toolsPanel.appendChild(editBtn);
+    toolsPanel.appendChild(deleteBtn);
+}
+const checkClick = (e) => {
+
+    if (e.target.closest('button').classList.contains('complete')) {
+        e.target.closest('li').classList.toggle('completed')
+        e.target.closest('button').classList.toggle('completed')
+    } else if (e.target.closest('button').className === 'edit'){ editTask();
+
+    }else if (e.target.closest('button').className === 'delete') {
+        console.log('delete');
+
+    }
 }
 
-document.addEventListener("DOMContentLoaded", main)// czekamy aż nasza strona musi się wczytać.
+const editTask = () =>{
+    $popup.style.display = 'flex'
+}
+const closePopup = () => {
+    $popup.style.display = 'none'
+}
+document.addEventListener("DOMContentLoaded", main)// czekamy na czytanie się całej strony 
