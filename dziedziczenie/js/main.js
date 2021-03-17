44167a248a871e74973ab43b77e523a3746da4f1
ASlawinska <slawinska.adrianna@gmail.com>
2021-03-17 15:12:40 +0100

    Object assign

--- dziedziczenie/js/main.js
+++ dziedziczenie/js/main.js
@@ -1,51 +1,15 @@
-// tworzenie obiektu na podstawie klasy 
-class Person{
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
-    sayHello(){
-        console.log(`${this.name} mówi cześć!`);
-    }
-};
-class Driver extends Person{// dzidziczenie 
-    constructor(name, age, licence){
-        super(name, age);// super musi być zawsze na gorze! kolejność arg musi być zachowana 
-        this.licence = licence;
-    }
-    isDriver(){
-        console.log(`${this.name} ma ${this.age} lata i posiada prawo jazdy kategorii ${this.licence}.`);
-    }
-};
-const person1 = new Person('Adela', 24);
-const driver1 = new Driver('Lily', 23, 'B');
-person1.sayHello();
-driver1.sayHello();
-driver1.isDriver();
-// dziedziczenie tylko w jedną stronę, dlatego klasa Driver ma metodę sayHello oraz isDriver, a Person nie posiada isDriver
-
-// cwiczenia 
-class Fruit {
-    constructor(name, place){
-        this.name = name;
-        this.place = place;
-    }
-    show(){
-        console.log(`nazwa:${this.name}, kraj pochodzenia: ${this.place} `);
-    }
-};
-class EgzoticFruit extends Fruit{
-    constructor(name, place, color) {
-        super(name, place);
-        this.color = color;
-    }
-    showColor(){
-        console.log(`kolor owoca egzotycznego to ${this.color}, kraj pochodzenia: ${this.place}.`);
-    }
-};
-const apple = new Fruit('apple', 'Polska');
-const banana = new EgzoticFruit('banana', 'Grecja', 'żółty');
-apple.show();
-banana.showColor();
-banana.show();
-
+const female = {
+    name: 'Ala',
+    age: 23,
+    hairColor: 'blond'
+}
+const men = {
+    name: 'Leszek',
+    age: 45,
+    hight: 199
+}
+const person = Object.assign({}, female, men);//{} - stworzenie nowego obiekty do którego zostają przypisane wartości dwóch pozostałych 
+// jezeli występuja dwa takie same klucze w obiektach laczonych, to wartości ostateczne w nowym obiekcie to te z obiektu najbardziej po prawej
+console.log(person);
+console.log(female);
+console.log(men);
