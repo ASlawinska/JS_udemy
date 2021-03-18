4fceb64975f10e2d70062afd6f30546d56b19d32
ASlawinska <slawinska.adrianna@gmail.com>
2021-03-18 13:31:55 +0100

    Added setters and getters

--- dziedziczenie/js/main.js
+++ dziedziczenie/js/main.js
@@ -1,15 +1,29 @@
-const female = {
-    name: 'Ala',
-    age: 23,
-    hairColor: 'blond'
-}
-const men = {
-    name: 'Leszek',
-    age: 45,
-    hight: 199
-}
-const person = Object.assign({}, female, men);//{} - stworzenie nowego obiekty do którego zostają przypisane wartości dwóch pozostałych 
-// jezeli występuja dwa takie same klucze w obiektach laczonych, to wartości ostateczne w nowym obiekcie to te z obiektu najbardziej po prawej
-console.log(person);
-console.log(female);
-console.log(men);
+//Get, używasz do pobierania wartości, a set do przypisania wartości. 
+let course = {
+    name: 'Podstawy JavaScript',
+    price: 50,
+    currency: 'PLN', 
+    priceInfo() {
+        return `Cena: ${course.price} ${course.currency}`;
+    }
+};
+
+console.log(course.priceInfo()); //Cena: 50 PLN
+let course = {
+    name: 'Podstawy JavaScript',
+    price: 50,
+    currency: 'PLN', 
+    get priceInfo() {
+        return `Cena: ${course.price} ${course.currency}`;
+    },
+    set priceInfo(value) {
+        let values = value.split(' ');
+        this.price = values[0];
+        this.currency = values[1];
+    }
+};
+
+console.log(course.priceInfo); //Cena: 50 PLN
+course.priceInfo = '20 USD';
+console.log(course.priceInfo); //Cena: 20 USD
+course.priceInfo = '20 USD';
